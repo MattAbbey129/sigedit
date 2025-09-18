ee71d28c76387bbaebbc6515ba0f7bde5887ab38
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-18 03:21:28 -0400

    feat: Add loop break if the message action prompt returns 'edit'
    Also added a new while loop to keep opening the text editor if the message isn't finished being edited yet.

--- script.bash
+++ script.bash
@@ -16,45 +16,52 @@ main() {
     local FILE_PATH="${HOME}/.local/state/sigedit/$(mktemp --dry-run "$(date +%Y%m%d%H%M%S)".XXXXXXXXXX)"
     readonly FILE_PATH
 
-    "${EDITOR}" "${FILE_PATH}"
+    local MESSAGE_FINISHED=false
+    while ! ${MESSAGE_FINISHED}; do
+        "${EDITOR}" "${FILE_PATH}"
 
-    if [[ ! -f "${FILE_PATH}" ]]; then
-        echo 'Message not saved'
-        exit 1
-    elif [[ -z "$(cat ${FILE_PATH})" || -z "$(cat ${FILE_PATH} | tr -d '[:space:]')" ]]; then
-        echo 'Message is empty'
-        exit 1
-    fi
-
-    echo -e "\n${FILE_PATH}"
-    echo -e '\n-----BEGIN PGP SIGNED MESSAGE-----\n'
-    cat "${FILE_PATH}"
-    echo -e '\n------END PGP SIGNED MESSAGE------\n'
-
-    echo 'Type the action to perform and press ENTER, or press CTRL+C to cancel'
-    echo '(Pressing ENTER without making a selection will default to: sign)'
-
-    local MESSAGE_ACTION=''
-
-    while true; do
-        printf '\n'
-        read -rp '[ s/sign | e/edit | c/cancel | d/delete ] ' MESSAGE_ACTION
-
-        if [[ -z "${MESSAGE_ACTION}" || "${MESSAGE_ACTION}" == 's' || "${MESSAGE_ACTION}" == 'sign' ]]; then
-            echo -e 'Signing message...\n'
-            break # from the 'while true' loop
-        elif [[ "${MESSAGE_ACTION}" == 'c' || "${MESSAGE_ACTION}" == 'cancel' ]]; then
-            exit 0
-        elif [[ "${MESSAGE_ACTION}" == 'd' || "${MESSAGE_ACTION}" == 'delete' ]]; then
-            local DELETION_ACTION=''
-            read -rp 'Are you sure you want to cancel and delete this message? [y/N] ' DELETEION_ACTION
-            if [[ "${DELETEION_ACTION}" == 'y' ]]; then
-                rm "${FILE_PATH}" && echo 'Message deleted'
-                exit 0
-            else
-                echo 'Not deleting message'
-            fi
+        if [[ ! -f "${FILE_PATH}" ]]; then
+            echo 'Message not saved'
+            exit 1
+        elif [[ -z "$(cat ${FILE_PATH})" || -z "$(cat ${FILE_PATH} | tr -d '[:space:]')" ]]; then
+            echo 'Message is empty'
+            exit 1
         fi
+
+        echo -e "\n${FILE_PATH}"
+        echo -e '\n-----BEGIN PGP SIGNED MESSAGE-----\n'
+        cat "${FILE_PATH}"
+        echo -e '\n------END PGP SIGNED MESSAGE------\n'
+
+        echo 'Type the action to perform and press ENTER, or press CTRL+C to cancel'
+        echo '(Pressing ENTER without making a selection will default to: sign)'
+
+        local MESSAGE_ACTION=''
+
+        while true; do
+            printf '\n'
+            read -rp '[ s/sign | e/edit | c/cancel | d/delete ] ' MESSAGE_ACTION
+
+            if [[ -z "${MESSAGE_ACTION}" || "${MESSAGE_ACTION}" == 's' || "${MESSAGE_ACTION}" == 'sign' ]]; then
+                local MESSAGE_FINISHED=true
+                echo -e 'Signing message...\n'
+                break # from this 'while true' loop
+            elif [[ "${MESSAGE_ACTION}" == 'e' || "${MESSAGE_ACTION}" == 'edit' ]]; then
+                echo 'Editing message...'
+                break # from this 'while true' loop
+            elif [[ "${MESSAGE_ACTION}" == 'c' || "${MESSAGE_ACTION}" == 'cancel' ]]; then
+                exit 0
+            elif [[ "${MESSAGE_ACTION}" == 'd' || "${MESSAGE_ACTION}" == 'delete' ]]; then
+                local DELETION_ACTION=''
+                read -rp 'Are you sure you want to cancel and delete this message? [y/N] ' DELETEION_ACTION
+                if [[ "${DELETEION_ACTION}" == 'y' ]]; then
+                    rm "${FILE_PATH}" && echo 'Message deleted'
+                    exit 0
+                else
+                    echo 'Not deleting message'
+                fi
+            fi
+        done
     done
 
     gpg --clear-sign --output "${FILE_PATH}.asc" "${FILE_PATH}"
