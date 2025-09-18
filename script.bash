11c839d9d2b8832d1e59cfb88e8f35d345d48bac
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-18 03:28:57 -0400

    refactor: extract editor checking to it's own function

--- script.bash
+++ script.bash
@@ -3,14 +3,18 @@
 # SPDX-License-Identifier: MIT
 # Copyright © 2025 Matt Abbey
 
-main() {
-
+check_if_editor_is_set() {
     if [[ -z "${EDITOR}" ]]; then
         echo '$EDITOR not set'
         exit 1
     else
         echo "Editing message with: ${EDITOR}"
     fi
+}
+
+main() {
+
+    check_if_editor_is_set
 
     mkdir -p "${HOME}/.local/state/sigedit"
     local FILE_PATH="${HOME}/.local/state/sigedit/$(mktemp --dry-run "$(date +%Y%m%d%H%M%S)".XXXXXXXXXX)"
